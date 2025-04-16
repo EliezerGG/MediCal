02db0128525e4bd0026d6049aa217a5701912763
Eliezer Guevara <magdiel.852@gmail.com>
2025-04-15 18:14:11 -0600

    proto

---
+++ CalendarioMensajesPrototipo/src/main/Main.java
@@ -0,0 +1,18 @@
+
+package main;
+
+import GUI.MainWindow;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindow mainWindow = new MainWindow();
+        
+        mainWindow.setVisible(true);
+        mainWindow.setLocationRelativeTo(null);
+    }
+    
+}
